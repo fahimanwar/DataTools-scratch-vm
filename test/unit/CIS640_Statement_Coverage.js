b179124cddd24d385970147d8a1e133fe1bafd66
fahimanwar <42549218+fahimanwar@users.noreply.github.com>
2020-04-15 17:50:36 -0500

    Update CIS640_Statement_Coverage.js

--- test/unit/CIS640_Statement_Coverage.js
+++ test/unit/CIS640_Statement_Coverage.js
@@ -5,6 +5,7 @@ const data = require('../../src/extensions/data_tools/index.js');
 const Runtime = require('../../src/engine/runtime');
 
 const dataset = [ {name:'mikey', age:25 }, {name:'joe', age:36}, {name:'steve', age:85}];
+const dataset2 = [];
 const fileName = "fileName";
 
 let runtime;
